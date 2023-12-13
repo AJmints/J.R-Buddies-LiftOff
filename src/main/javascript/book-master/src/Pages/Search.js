b29e73741094646f20715aa990f480984a0e899b
may2fn <may2fn@umsystem.edu>
2023-12-12 20:12:09 -0600

    Started implementing external api

--- src/main/javascript/book-master/src/Pages/Search.js
+++ src/main/javascript/book-master/src/Pages/Search.js
@@ -1,21 +1,25 @@
 import react, {useState} from "react";
+import axios from "axios";
 
 const Search=()=>{
     const [search, setSearch] = useState("");
-    const searchBook=(evt)=>{
-        if(evt.key==="Enter"){
-            console.log("Search Confirmed");
-        }
+
+    const searchBook = (e) => {
+        e.preventDefault();
+        axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyBp48rUKYmtTfkAGAktCqmsmgOZAjRxR3g')
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err));
     }
 
     return(
         <>
             <div>
                 <h2>Search Books</h2>
-                <div>
-                    <input type="text" placeholder="Enter Book Name" value={search} onChange={e=>setSearch(e.target.value)} onKeyUp={searchBook}/>
+                <form onSubmit = {searchBook}>
+                    <input type="text" placeholder="Enter Book Name" value={search}
+                        onChange={e=>setSearch(e.target.value)}/>
                     <button type="submit">Submit</button>
-                </div>
+                </form>
             </div>
         </>)
 }
