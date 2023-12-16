5ddf8c6dc98a7dd8c1a46029e1d837960c1c1d93
Chad Harper <chadharper811@gmail.com>
2023-12-15 22:06:44 -0600

    minor updates to layout

--- src/main/javascript/book-master/src/pages/Layout.js
+++ src/main/javascript/book-master/src/pages/Layout.js
@@ -2,6 +2,7 @@ import { Outlet, Link } from "react-router-dom";
 import Header from "../components/header";
 import "./layout.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Collapse } from "bootstrap";
 
 const Layout = () => {
     return (
@@ -10,36 +11,36 @@ const Layout = () => {
                 <Header />
             </header>
             
-            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <Link to="/" className="nav-link">Home</Link>
                     </li>
+
                     <li className="nav-item">
                         <Link to="advanced_search" className="nav-link">Advanced Search</Link>
                     </li>
+
                     <form className="d-flex">
-                        <input className="form-control me-2" type="text" placeholder="Quick Search"></input>
+                        <input className="form-control me-2" type="text" placeholder="Quick Search" id="search_keyword"></input>
                         <button className="btn btn-primary" type="button">Search</button>
                     </form>
+
                     <li className="nav-item">
                         <Link to="user_sign_in" className="nav-link">Login</Link>
                     </li>
+
+{/* last 2 pages hidden will add code for with user verification and link will be visible only to correct user type */}
+
                     <li className="nav-item invisible">
                         <Link to="customer_account" className="nav-link">Customer Account</Link>
                     </li>
+
                     <li className="nav-item invisible">
                         <Link to="admin_account" className="nav-link">Admin Account</Link>
                     </li>
-                        
-                        
 
-                        
-
-                        
-                        
                 </ul>
-
             </nav>
 
             <main>
@@ -47,6 +48,9 @@ const Layout = () => {
             </main>
 
             <footer>
+                <div className="navbar navbar-expand-sm bg-dark navbar-dark fixed-bottom">
+                    <span className="navbar-text">JR-Buddies &copy; </span>
+                </div>
             </footer>
         </div>
     )
