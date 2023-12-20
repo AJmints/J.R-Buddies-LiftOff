5821310a7eb0401c850135d1ea2d98b1fcdd53fd
Melany Willis <melexiw@gmail.com>
2023-12-19 21:07:22 -0600

    Added useState, no backend to connect to

--- src/main/javascript/book-master/src/pages/UserSignin.js
+++ src/main/javascript/book-master/src/pages/UserSignin.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 function UserSignIn() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("localhost:8080", {
+                username,
+                password,
+            });
+
+            if (response.status === 200) {
+                console.log("Login successful");
+            } else {
+                console.error("Authentication failed");
+            }
+        } catch (error) {
+            console.error("Error during login:", error);
+        }
+    };
+
     return (
         <div className='container mt-5'>
             <h3>Log In</h3>
-                <form className='row g-3'>
+                <form className='row g-3' onSubmit={handleSubmit}>
                     <div className='col-md-6'>
                         <label htmlFor='username' className='form-label'>User Name:</label>
-                        <input type='text' className='form-control' id='username' required/>
+                        <input type='text' className='form-control' id='username' required
+                        value={username} onChange={(e) => setUsername(e.target.value)}/>
                     </div>
                     <div className='col-md-6'>
                         <label htmlFor="password" className='form-label'>Password:</label>
-                        <input type='password' className='form-control' id='password' required/>
+                        <input type='password' className='form-control' id='password' required
+                        value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     <div>
                         <button type='submit' className="btn btn-success">Submit</button>
