85478bdc427fb2b79dda9aead850655353583ddb
Chad Harper <chadharper811@gmail.com>
2023-12-22 20:55:59 -0600

    bootrun changes

--- src/main/javascript/book-master/src/components/ReviewAndRating.js
+++ src/main/javascript/book-master/src/components/ReviewAndRating.js
@@ -1,5 +1,5 @@
+import React , {useState} from "react";
 import axios from "axios";
-import { useState } from "react";
 
 function ReviewAndRating () {
     const [book, setBook] = useState("");
@@ -8,53 +8,50 @@ function ReviewAndRating () {
     const [rating, setRating] = useState("");
 
 
-    const handleInputChange = (e, setStateFunction) => {
-        setStateFunction(e.target.value);
-    }
+    // const handleInputChange = (e, setStateFunction) => {
+    //     setStateFunction(e.target.value);
+    // };
 
     const submitReview = async (e) => {
         e.preventDefault();
         console.log("Submited");
 
-        axios.post("http://localhost:8080/reviews", {book, user, review, rating})
-        .then(response => console.log(response))
-        .catch(err => console.log(err))
-        // try{
-        //     const response = await axios.post("/reviews", {
-        //         book,
-        //         user,
-        //         review,
-        //         rating
-        //     });
+        try{
+            const response = await axios.post("http://localhost:8080/reviews", {
+                book,
+                user,
+                review,
+                rating,
+            });
 
-        //     if (response.status === 200) {
+            if (response.status === 200) {
 
-        //     } else {
+            } else {
 
-        //     }
-        // } catch (error) {
-        //     console.error("Error posting review", error)
-        // }
+            }
+        } catch (error) {
+            console.error("Error posting review", error)
+        }
     };
 
-    return <div>
+    return <div className="container mt-5">
                 <h3>Leave a review:</h3>
-                <form onSubmit={submitReview}>
+                <form className="row g-3" onSubmit={submitReview}>
                     <div className='row mt-2'>
                         <div className='col-6'>
                             <label htmlFor="book" className='form-label'>Book Title:</label>
-                            <input type="text" className='form-control' id="book" name="book" placeholder="Dune" value={book} onChange={(e) => handleInputChange(e, setBook)} required/>
+                            <input type="text" className='form-control' id="book" name="book" placeholder="Dune" value={book} onChange={(e) => setBook(e.target.value)} required/>
                         </div>
                     </div>
                     <div className='row mt-2'>
                         <div className='col-6'>
                             <label htmlFor="user" className='form-label'>User ID:</label>
-                            <input type="text" className='form-control' id="user" name="user" placeholder="example@gmail.com" value={user} onChange={(e) => handleInputChange(e, setUser)} required/>
+                            <input type="text" className='form-control' id="user" name="user" placeholder="example@gmail.com" value={user} onChange={(e) => setUser(e.target.value)} required/>
                         </div>
                     </div>
                     <div className='col-3 mt-2'>
                         <label htmlFor='rating' className='form-label'>Rating: </label>
-                        <select className='form-control' id='rating' name='rating' defaultValue="5 stars" value={rating} onChange={(e) => handleInputChange(e, setRating)} required>
+                        <select className='form-control' id='rating' name='rating' value={rating} onChange={(e) => setRating(e.target.value)} required>
                             <option></option>
                             <option>5 Stars</option>
                             <option>4 Stars</option>
@@ -65,7 +62,7 @@ function ReviewAndRating () {
                     </div>
                     <div className='row mt-3'>
                         <div className='form-floating'>
-                            <textarea className='form-control' id="review" name='review' placeholder='Write review here' rows={5} value={review} onChange={(e) => handleInputChange(e, setReview)}/>
+                            <textarea className='form-control' id="review" name='review' placeholder='Write review here' rows={5} value={review} onChange={(e) => setReview(e.target.value)}/>
                             <label className="ms-2" htmlFor='review' >Review:</label>
                         </div>
                     </div>
