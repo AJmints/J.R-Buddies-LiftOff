d36f3b1f5e16bc9942188af67958646385f0cc3f
may2fn <may2fn@umsystem.edu>
2024-01-05 19:01:42 -0600

    Started working on removing recommendations

---
+++ src/main/javascript/book-master/src/pages/RemoveRecommendations.js
@@ -0,0 +1,63 @@
+import React , {useState, useRef} from 'react';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const RemoveRecommendation=()=> {
+    const [userID, setUserID] = useState(0);
+    const [password, setPassword] = useState("");
+    const isValid = useRef(false);
+    const [recommendData, setRecommendData] = useState([]);
+    const [users, setUsers] = useState([]);
+    const show = useRef(false);;
+    const navigate = useNavigate();
+
+    const checkUserInfo = () => {
+        users.map((user) => {
+            if(user.id == userID && user.password.equals(password)){
+                isValid.current = true;
+            }
+        })
+    }
+
+    const createRecommendation = (e) => {
+        e.preventDefault();
+
+        axios.get("http://localhost:8080/api/user/all")
+            .then(res=>setUsers(res.data))
+            .catch(err=>console.log(err));
+
+        checkUserInfo();
+
+        if(isValid.current){
+            axios.get("http://localhost:8080/recommendation/search?idType="+ "user"+"&idValue="+userID)
+                .then(res=>setRecommendData(res.data))
+                .catch(err=>console.log(err));
+            show.current = true;
+        }
+    }
+
+    return(
+            <>
+                <div className='container mt-3'>
+                    <h2>Enter User ID & Password </h2>
+                    <form onSubmit = {createRecommendation}>
+                        <label> User ID:
+                        <input type="number" placeholder="ID Number" value={userID}
+                            onChange={e=>setUserID(e.target.value)}/>
+                        </label>
+                        <br></br>
+                        <label> Password:
+                        <input type="text" placeholder="Enter Password" value={password}
+                            onChange={e=>setPassword(e.target.value)}/>
+                        </label>
+                        <button type="submit">Submit ID</button>
+                    </form>
+                </div>
+
+                <div id="resultsDiv" style={{display: (show ? 'block' : 'none')}}>
+                    <DeleteRecommendation results={recommendData} />
+                </div>
+            </>)
+}
+
+export default RemoveRecommendation;
