99d197b55d662e30c1f180a02a6d83129318a5d4
Chad Harper <chadharper811@gmail.com>
2024-01-08 00:27:08 -0600

    got update review section working

--- src/main/javascript/book-master/src/pages/CustomerAccount.js
+++ src/main/javascript/book-master/src/pages/CustomerAccount.js
@@ -2,7 +2,7 @@ import CustomerReviews from "../components/CustomerReviews";
 
 const CustomerAccount = () => {
     return <div className="container mt-5">
-        <h1>CustomerAccount</h1>
+        <h1>Customer Account</h1>
         <div>
             <CustomerReviews />
         </div>
